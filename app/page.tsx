e3f801f641ed3f59a5084d6b07fef2ccefe4e0c3
imgustacabral <imgustacabral@gmail.com>
2023-07-07 17:28:18 -0300

    feat: add scripts `lint:check` and `lint:fix`

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-white font-bold text-4xl">Site em construção</h1>
     </main>
-  )
+  );
 }
